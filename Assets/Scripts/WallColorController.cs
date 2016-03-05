6c386b8f8c42148f6bdcf47b3eb3805e86710fed
John Doyle <john.c.doyle@gmail.com>
2016-03-05 13:37:47 -0500

    Changing wall colors on emotions

---
+++ Assets/Scripts/WallColorController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallColorController : MonoBehaviour 
+{	
+	private GameObject[] wallTops;
+	private Component[] topRender;
+
+	public Material sadMaterial;
+	public Material angryMaterial;
+	public Material happyMaterial;
+	public Material surpriseMaterial;
+
+	void Start()
+	{
+		if (wallTops == null) {
+			wallTops = GameObject.FindGameObjectsWithTag ("top");
+		}
+	}
+
+	void Awake()
+	{
+		EventController.Instance.Subscribe<GoWestEvent> (GoWest);
+		EventController.Instance.Subscribe<GoEastEvent> (GoEast);
+		EventController.Instance.Subscribe<GoNorthEvent> (GoNorth);
+		EventController.Instance.Subscribe<GoSouthEvent> (GoSouth);
+	}
+
+	void GoWest(GoWestEvent eventTest) {
+		foreach (GameObject top in wallTops) {
+			topRender = top.GetComponentsInChildren<Renderer>();
+			foreach (Renderer rend in topRender) 
+			{
+				rend.material.Lerp(rend.material, surpriseMaterial, 0.5f);
+			}
+		}
+	}
+
+	void GoEast(GoEastEvent eventTest) {
+		foreach (GameObject top in wallTops) {
+			topRender = top.GetComponentsInChildren<Renderer>();
+			foreach (Renderer rend in topRender) 
+			{
+				rend.material.Lerp(rend.material, angryMaterial, 0.5f);
+			}
+		}
+	}
+
+	void GoNorth(GoNorthEvent eventTest) {
+		foreach (GameObject top in wallTops) {
+			topRender = top.GetComponentsInChildren<Renderer>();
+			foreach (Renderer rend in topRender) 
+			{
+				rend.material.Lerp(rend.material, happyMaterial, 0.5f);
+			}
+		}
+	}
+
+	void GoSouth(GoSouthEvent eventTest) {
+
+		foreach (GameObject top in wallTops) {
+			topRender = top.GetComponentsInChildren<Renderer>();
+			foreach (Renderer rend in topRender) 
+			{
+				rend.material.Lerp(rend.material, sadMaterial, 0.5f);
+			}
+		}
+	}
+
+
+}
