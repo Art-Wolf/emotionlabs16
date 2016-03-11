1e3f8d6c6bdcff6638f61f4297ccc83d413cefca
Frank O\'Connor <fjo.con@gmail.com>
2016-03-10 22:58:08 -0500

    Added basic instructions screen

---
+++ Assets/Scripts/Intro/InstructionsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsViewModel : MonoBehaviour {
+
+	public void ShowInstructionsPanel () {
+		this.transform.SetAsLastSibling ();
+	}
+
+	public void HideInstructionsPanel () {
+		this.transform.SetAsFirstSibling ();
+	}
+}
